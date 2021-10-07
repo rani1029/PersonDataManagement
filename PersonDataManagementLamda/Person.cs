ff72626dd2f3b626ea56c5566678a31d2d478ec3
rani1029 <ranig1029@gmail.com>
2021-10-07 19:42:46 +0530

    Welcome msg added

---
+++ PersonDataManagementLamda/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDataManagementLamda
+{
+    class Person
+    {
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
