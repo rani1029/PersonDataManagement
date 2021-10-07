fcb0f9be5d92f92095c57ef226de610a718b5c72
rani1029 <ranig1029@gmail.com>
2021-10-07 22:09:25 +0530

    Retrieve top two records of age less than 60 is done using lambda expresion

--- PersonDataManagementLamda/Program.cs
+++ PersonDataManagementLamda/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PersonDataManagementLamda
 {
@@ -8,30 +10,54 @@ namespace PersonDataManagementLamda
         {
             Console.WriteLine("Welcome to person data Management!");
             //creating list of person 
-            List<Person> Personlist=new List<Person>();
+            List<Person> Personlist = new List<Person>();
             AddPersonDetails(Personlist);
-            
+            IteratePersoninList(Personlist);
+            RetrieveTopTwoRecords(Personlist);
+
 
         }
         //Add Default values to list
         public static void AddPersonDetails(List<Person> Personlist)
-            {
-            Personlist.Add(new Person(){SSN = 1, Name = "Rohit", Address = "Delhi", Age = 25});
-            Personlist.Add(new Person(){SSN = 2, Name = "vikash", Address = "Deheradun", Age = 65});
-            Personlist.Add(new Person(){SSN = 3, Name = "john", Address = "Dispur", Age = 23});
-            Personlist.Add(new Person(){SSN = 4, Name = "Mark", Address = "Mumbai", Age = 18});
-            Personlist.Add(new Person(){SSN = 5, Name = "rashi", Address = "Kolkta", Age = 16});
-            Personlist.Add(new Person(){SSN = 6, Name = "Sumit", Address = "chennai", Age = 68});
+        {
+            Personlist.Add(new Person() { SSN = 1, Name = "Rohit", Address = "Delhi", Age = 25 });
+            Personlist.Add(new Person() { SSN = 2, Name = "vikash", Address = "Deheradun", Age = 65 });
+            Personlist.Add(new Person() { SSN = 3, Name = "john", Address = "Dispur", Age = 23 });
+            Personlist.Add(new Person() { SSN = 4, Name = "Mark", Address = "Mumbai", Age = 18 });
+            Personlist.Add(new Person() { SSN = 5, Name = "rashi", Address = "Kolkta", Age = 16 });
+            Personlist.Add(new Person() { SSN = 6, Name = "Sumit", Address = "chennai", Age = 68 });
         }
         //display Person details in the List
         public static void IteratePersoninList(List<Person> Personlist)
+        {
+            foreach (Person person in Personlist)
             {
-            foreach(Person person in Personlist)
-                {
-                Console.WriteLine("SSN={0}\tName={1}\tAddress={2}\tAge={3})",person.SSN,person.Name,person.Address,person.Age);
+                Console.WriteLine("SSN={0}\tName={1}\tAddress={2}\tAge={3})", person.SSN, person.Name, person.Address, person.Age);
             }
         }
+        //UC-2 to get Top 2 records age less than 60
+        public static void RetrieveTopTwoRecords(List<Person> Personlist)
+        {
+            ////Age less than 60
+            //List<Person> AgeLessThanSixty = Personlist.FindAll(person => person.Age < 60);
+            //Console.WriteLine("Displaying person details if age is less than 60");
+            //IteratePersoninList(AgeLessThanSixty);
+            ////sorted list
+            //List<Person> AsecendingList = AgeLessThanSixty.OrderBy(age => age.Age).ToList();
+            //Console.WriteLine(" person details after sorting by Age");
+            //IteratePersoninList(AsecendingList);
+            ////sorted top two
+            //List<Person> TopTwo = AsecendingList.Take(2).ToList();
+            //Console.WriteLine("Top Two records of Age less than sixty");
+            //IteratePersoninList(TopTwo);
+            //All 3 can steps in one Line 
+            List<Person> SortedResult = Personlist.FindAll(person => person.Age < 60).OrderBy(age => age.Age).Take(2).ToList();
+            Console.WriteLine("Top Two records of Age less than sixty");
+            IteratePersoninList(SortedResult);
 
-           
+        }
+
+
+
     }
 }
